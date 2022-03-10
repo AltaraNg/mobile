ed9c66edc9206ece9f5d922a1cab90c835ce5c9c
Cheedinmar <43110334+cheedinmar@users.noreply.github.com>
2022-03-10 10:15:17 +0100

    UI fix (#13)
    * Modal on dashboard fix
    
    * done
    
    Co-authored-by: Damola Oladeji <darmhoo@hotmail.com>

--- screens/EditProfile.tsx
+++ screens/EditProfile.tsx
@@ -1,244 +1,257 @@
 import {
-  Pressable,
-  StyleSheet,
-  TextInput,
-  ActivityIndicator,
-  ToastAndroid,
-  BackHandler,
-  Platform,
-  TouchableOpacity,
-  Modal,
-  Alert,
-  Dimensions,
-} from "react-native";
+	Pressable,
+	StyleSheet,
+	TextInput,
+	ActivityIndicator,
+	ToastAndroid,
+	BackHandler,
+	Platform,
+	TouchableOpacity,
+	Modal,
+	Alert,
+	Dimensions,
+} from 'react-native';
 
-import { LinearGradient } from "expo-linear-gradient";
-import { SuccessSvg, FailSvg, LogOut } from "../assets/svgs/svg";
-import Header from "../components/Header";
-import React, { useState, createRef, useEffect, useContext } from "react";
-import Hamburger from "../assets/svgs/hamburger.svg";
-import { Text, View } from "../components/Themed";
+import { LinearGradient } from 'expo-linear-gradient';
+import { SuccessSvg, FailSvg, LogOut } from '../assets/svgs/svg';
+import Header from '../components/Header';
+import React, { useState, createRef, useEffect, useContext } from 'react';
+import Hamburger from '../assets/svgs/hamburger.svg';
+import { Text, View } from '../components/Themed';
 import {
-  DrawerParamList,
-  RootStackParamList,
-  RootTabParamList,
-} from "../types";
-import Cards from "../components/Cards";
-import SideMenu from "./SideMenu";
-import { Context as AuthContext } from "../context/AuthContext";
-import { NativeStackScreenProps } from "@react-navigation/native-stack";
-import { UserInterfaceIdiom } from "expo-constants";
-import axios from "axios";
+	DrawerParamList,
+	RootStackParamList,
+	RootTabParamList,
+} from '../types';
+import Cards from '../components/Cards';
+import SideMenu from './SideMenu';
+import { Context as AuthContext } from '../context/AuthContext';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import { UserInterfaceIdiom } from 'expo-constants';
+import axios from 'axios';
 
-type Props = NativeStackScreenProps<RootTabParamList, "Dashboard">;
+type Props = NativeStackScreenProps<RootTabParamList, 'Dashboard'>;
 import Constants from 'expo-constants';
-
 
 let url = Constants?.manifest?.extra?.URL;
 axios.defaults.baseURL = url;
 
 export default function Dashboard({ navigation, route }: Props) {
-  const { state } = useContext(AuthContext);
-  const [exitApp, setExitApp] = useState(1);
-  const [isError, setIsError] = useState(false);  
-  const [user, setUser] = useState(state.user.attributes);
-  const [showMenu, setShowMenu] = useState(false);
-  const toggleSideMenu = async () => {
-    navigation.toggleDrawer();
-  };
-  let successMessage = "You have successfully applied for  an E-loan";
-  let errorMessage = "Sorry! Your Order is unsuccessful";
+	const { state } = useContext(AuthContext);
+	const [exitApp, setExitApp] = useState(1);
+	const [isError, setIsError] = useState(false);
+	const [user, setUser] = useState(null);
+	const [showMenu, setShowMenu] = useState(false);
+	const toggleSideMenu = async () => {
+		navigation.toggleDrawer();
+	};
+	let successMessage = 'You have successfully applied for  an E-loan';
+	let errorMessage = 'Sorry! Your Order is unsuccessful';
 
-  const backAction = () => {
-    if (Platform.OS === "ios") return;
-    setTimeout(() => {}, 3000);
+	const backAction = () => {
+		if (Platform.OS === 'ios') return;
+		setTimeout(() => {}, 3000);
 
-    if (exitApp === 0) {
-      setExitApp(exitApp + 1);
+		if (exitApp === 0) {
+			setExitApp(exitApp + 1);
 
-      ToastAndroid.showWithGravity(
-        "press back button again to exit app",
-        ToastAndroid.SHORT,
-        ToastAndroid.CENTER
-      );
-    } else {
-      BackHandler.exitApp();
-    }
+			ToastAndroid.showWithGravity(
+				'press back button again to exit app',
+				ToastAndroid.SHORT,
+				ToastAndroid.CENTER
+			);
+		} else {
+			BackHandler.exitApp();
+		}
 
-    return true;
-  };
+		return true;
+	};
 
-  
-  async function handleUpdate (){
-    try {
-      let result = await axios({
-        method: 'PATCH',
-        url: '/customers',
-        headers: { 'Authorization': `Bearer ${state.token}` },
-        data: user
-      });
-    } catch (error) {
-    }
-    let result = await axios({
-      method: 'PATCH',
-      url: '/customers',
-      headers: { 'Authorization': `Bearer ${state.token}` },
-      data: user
-    });
-    if(result){
-    }
-  }
+	const handleUpdate = async () => {
+		try {
+			let result = await axios({
+				method: 'PATCH',
+				url: `/customers/${state.user.id}`,
+				headers: { Authorization: `Bearer ${state.token}` },
+				data: user,
+			});
+			ToastAndroid.showWithGravity(
+				'Profile updated successfully',
+				ToastAndroid.SHORT,
+				ToastAndroid.CENTER
+			);
+      navigation.navigate('View Profile');
+		} catch (error) {
+			ToastAndroid.showWithGravity(
+				'Error! Request was not completed',
+				ToastAndroid.SHORT,
+				ToastAndroid.CENTER
+			);
+		}
+	};
 
-  useEffect(() => {
-    const backHandler = BackHandler.addEventListener(
-      "hardwareBackPress",
-      backAction
-    );
-    return () => backHandler.remove();
-  }, []);
+	const fetchUser = async () => {
+		try {
+			let response = await axios({
+				method: 'GET',
+				url: `/auth/user`,
+				headers: { 'Authorization': `Bearer ${state.token}` },
+			});
+			const user = response.data.data[0].attributes;
+			setUser(user);
+		} catch (error: any) {
+			ToastAndroid.showWithGravity(
+				'Unable to fetch user',
+				ToastAndroid.SHORT,
+				ToastAndroid.CENTER
+			);
+		}
+	};
 
-  const logout = () => {
-    navigation.navigate("Login");
-  };
-  return (
-    <View style={styles.container}>
-      {showMenu && <SideMenu Logout="Logout" />}
-      <View style={styles.header}>
-        <Header></Header>
-        <TouchableOpacity>
-          <Pressable onPress={toggleSideMenu}>
-            <Hamburger style={styles.hamburger} />
-          </Pressable>
-        </TouchableOpacity>
-      </View>
+	useEffect(() => {
+		fetchUser();
+	}, []);
 
-      <View style={styles.main}>
-        <Text style={styles.title}>Edit Profile</Text>
-        <View style={styles.data}>
-          <Text style={styles.label}>First Name</Text>
-          <TextInput
-            style={styles.input}
-            value={user.first_name}
-            onChangeText={(txt) => setUser ({...user, first_name:txt})}
-          ></TextInput>
-        </View>
-        <View style={styles.data}>
-          <Text style={styles.label}>Last Name</Text>
-          <TextInput style={styles.input}
-          
-          onChangeText={(txt) => setUser ({...user, last_name:txt})}
-          >
-            {user.last_name}
-          </TextInput>
-        </View>
-        <View style={styles.data}>
-          <Text style={styles.label}>Phone Number</Text>
-          <TextInput style={styles.input}
-            onChangeText={(txt) => setUser ({...user, phone_number:txt})}
-            
-          >
-            {user.phone_number}
-          </TextInput>
-        </View>
-        <View style={styles.data}>
-          <Text style={styles.label}>Email Address</Text>
-          <TextInput style={styles.input}
-            onChangeText={(txt) => setUser ({...user, email:txt})}
-          
-          >
-            {user.email_address}
-          </TextInput>
-        </View>
-        <LinearGradient
-          colors={["#074A77", "#089CA4"]}
-          style={styles.buttonContainer}
-          start={{ x: 1, y: 0.5 }}
-          end={{ x: 0, y: 0.5 }}
-        >
-          <Pressable style={[styles.button]} onPress={handleUpdate}>
-            <Text style={styles.buttonText}>Save</Text>
-          </Pressable>
-        </LinearGradient>
-      </View>
-    </View>
-  );
+	return (
+		<View style={styles.container}>
+			{showMenu && <SideMenu Logout="Logout" />}
+			<View style={styles.header}>
+				<Header></Header>
+				<TouchableOpacity>
+					<Pressable onPress={toggleSideMenu}>
+						<Hamburger style={styles.hamburger} />
+					</Pressable>
+				</TouchableOpacity>
+			</View>
+			{user && (
+				<View style={styles.main}>
+					<Text style={styles.title}>Edit Profile</Text>
+					<View style={styles.data}>
+						<Text style={styles.label}>First Name</Text>
+						<TextInput
+							style={styles.input}
+							value={user.first_name}
+							onChangeText={(txt) => setUser({ ...user, first_name: txt })}
+						></TextInput>
+					</View>
+					<View style={styles.data}>
+						<Text style={styles.label}>Last Name</Text>
+						<TextInput
+							style={styles.input}
+							onChangeText={(txt) => setUser({ ...user, last_name: txt })}
+						>
+							{user.last_name}
+						</TextInput>
+					</View>
+					<View style={styles.data}>
+						<Text style={styles.label}>Phone Number</Text>
+						<TextInput
+							style={styles.input}
+							onChangeText={(txt) => setUser({ ...user, phone_number: txt })}
+						>
+							{user.phone_number}
+						</TextInput>
+					</View>
+					<View style={styles.data}>
+						<Text style={styles.label}>Email Address</Text>
+						<TextInput
+							style={styles.input}
+							onChangeText={(txt) => setUser({ ...user, email: txt })}
+						>
+							{user.email_address}
+						</TextInput>
+					</View>
+					<LinearGradient
+						colors={['#074A77', '#089CA4']}
+						style={styles.buttonContainer}
+						start={{ x: 1, y: 0.5 }}
+						end={{ x: 0, y: 0.5 }}
+					>
+						<Pressable style={[styles.button]} onPress={handleUpdate}>
+							<Text style={styles.buttonText}>Save</Text>
+						</Pressable>
+					</LinearGradient>
+				</View>
+			)}
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    height: "100%",
-    position: "relative",
-  },
-  buttonContainer: {
-    flexDirection: "row",
-    borderColor: "#074A74",
-    borderWidth: 1,
-    borderRadius: 10,
-    width: 250,
-    paddingVertical: 10,
-    marginHorizontal: Dimensions.get("window").width * 0.15,
-  },
-  button: {
-    flex: 1,
-    alignItems: "center",
-    borderRadius: 4,
-  },
-  buttonText: {
-    color: "#ffffff",
-    fontFamily: "Montserrat_600SemiBold",
-    textAlign: "center",
-    fontSize: 18,
-  },
-  input: {
-    backgroundColor: "#E8EBF7",
-    color: "#72788D",
-    marginRight: 25,
-    paddingVertical: 13,
-    borderColor: "#222",
-    borderRadius: 10,
-    paddingHorizontal: 10,
-    fontSize: 15,
-    fontFamily: "Montserrat_600SemiBold",
-  },
-  label: {
-    color: "#000",
-    fontFamily: "Montserrat_500Medium",
-    paddingBottom: 3,
-  },
-  data: {
-    backgroundColor: "#EFF5F9",
-    paddingLeft: 30,
-    marginBottom: 30,
-  },
-  hamburger: {
-    marginTop: 80,
-    marginRight: 24,
-  },
-  cards: {
-    backgroundColor: "#EFF5F9",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  header: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    backgroundColor: "#EFF5F9",
-  },
-  main: {
-    flex: 3,
-    backgroundColor: "#EFF5F9",
-  },
-  title: {
-    marginHorizontal: 30,
-    fontSize: 25,
-    color: "#074A74",
-    fontFamily: "Montserrat_700Bold",
-    marginBottom: 45,
-  },
-  menu: {
-    position: "absolute",
-    right: 0,
-  },
+	container: {
+		flex: 1,
+		height: '100%',
+		position: 'relative',
+	},
+	buttonContainer: {
+		flexDirection: 'row',
+		borderColor: '#074A74',
+		borderWidth: 1,
+		borderRadius: 10,
+		width: 250,
+		paddingVertical: 10,
+		marginHorizontal: Dimensions.get('window').width * 0.15,
+	},
+	button: {
+		flex: 1,
+		alignItems: 'center',
+		borderRadius: 4,
+	},
+	buttonText: {
+		color: '#ffffff',
+		fontFamily: 'Montserrat_600SemiBold',
+		textAlign: 'center',
+		fontSize: 18,
+	},
+	input: {
+		backgroundColor: '#E8EBF7',
+		color: '#72788D',
+		marginRight: 25,
+		paddingVertical: 13,
+		borderColor: '#222',
+		borderRadius: 10,
+		paddingHorizontal: 10,
+		fontSize: 15,
+		fontFamily: 'Montserrat_600SemiBold',
+	},
+	label: {
+		color: '#000',
+		fontFamily: 'Montserrat_500Medium',
+		paddingBottom: 3,
+	},
+	data: {
+		backgroundColor: '#EFF5F9',
+		paddingLeft: 30,
+		marginBottom: 30,
+	},
+	hamburger: {
+		marginTop: 80,
+		marginRight: 24,
+	},
+	cards: {
+		backgroundColor: '#EFF5F9',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	header: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		backgroundColor: '#EFF5F9',
+	},
+	main: {
+		flex: 3,
+		backgroundColor: '#EFF5F9',
+	},
+	title: {
+		marginHorizontal: 30,
+		fontSize: 25,
+		color: '#074A74',
+		fontFamily: 'Montserrat_700Bold',
+		marginBottom: 45,
+	},
+	menu: {
+		position: 'absolute',
+		right: 0,
+	},
 });
--- screens/ViewProfile.tsx
+++ screens/ViewProfile.tsx
@@ -29,6 +29,7 @@ import SideMenu from './SideMenu';
 import { Context as AuthContext } from '../context/AuthContext';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
 import { DrawerScreenProps } from '@react-navigation/drawer';
+import axios from 'axios';
 
 type Props = DrawerScreenProps<DrawerParamList, 'View Profile'>;
 
@@ -36,14 +37,35 @@ export default function ViewProfile({ navigation, route }: Props) {
 	const { state } = useContext(AuthContext);
 	const [exitApp, setExitApp] = useState(1);
 	const [showMenu, setShowMenu] = useState(false);
+	const [user, setUser] = useState(null);
 	const toggleSideMenu = async () => {
 		navigation.toggleDrawer();
 	};
 
+	const fetchUser = async () => {
+		try {
+			let response = await axios({
+				method: 'GET',
+				url: `/auth/user`,
+				headers: { 'Authorization': `Bearer ${state.token}` },
+			});
+			const user = response.data.data[0];
+			setUser(user);
+		} catch (error: any) {
+			ToastAndroid.showWithGravity(
+				'Unable to fetch user',
+				ToastAndroid.SHORT,
+				ToastAndroid.CENTER
+			);
+		}
+	};
+
+	useEffect(() => {
+		fetchUser();
+	}, []);
+
 	return (
 		<View style={styles.container}>
-			{showMenu && <SideMenu Logout="Logout" />}
-
 			<View style={styles.header}>
 				<Header></Header>
 				<TouchableOpacity>
@@ -52,177 +74,173 @@ export default function ViewProfile({ navigation, route }: Props) {
 					</Pressable>
 				</TouchableOpacity>
 			</View>
-
-			<View style={styles.main}>
-				<Text style={styles.name}>My Profile</Text>
-				<View
-					style={{
-						justifyContent: 'center',
-						alignItems: 'center',
-						backgroundColor: '#EFF5F9',
-						marginTop: 20,
-					}}
-				>
-					<TouchableHighlight
+			{user && (
+				<View style={styles.main}>
+					<Text style={styles.name}>My Profile</Text>
+					<View
 						style={{
-							borderRadius:
-								Math.round(
-									Dimensions.get('window').width +
-										Dimensions.get('window').height
-								) / 2,
-							width: Dimensions.get('window').width * 0.2,
-							height: Dimensions.get('window').width * 0.2,
-							backgroundColor: '#074A74',
 							justifyContent: 'center',
 							alignItems: 'center',
+							backgroundColor: '#EFF5F9',
+							marginTop: 20,
 						}}
-						underlayColor="#ccc"
-						
 					>
-						<Text
+						<TouchableHighlight
 							style={{
-								fontSize: 38,
-								color: '#fff',
-								fontFamily: 'Montserrat_800ExtraBold',
+								borderRadius:
+									Math.round(
+										Dimensions.get('window').width +
+											Dimensions.get('window').height
+									) / 2,
+								width: Dimensions.get('window').width * 0.2,
+								height: Dimensions.get('window').width * 0.2,
+								backgroundColor: '#074A74',
+								justifyContent: 'center',
+								alignItems: 'center',
 							}}
+							underlayColor="#ccc"
 						>
-							{' '}
-							{state.user.attributes.first_name.charAt(0).toUpperCase()}{' '}
-						</Text>
-					</TouchableHighlight>
-				</View>
+							<Text
+								style={{
+									fontSize: 38,
+									color: '#fff',
+									fontFamily: 'Montserrat_800ExtraBold',
+								}}
+							>
+								{' '}
+								{state.user.attributes.first_name.charAt(0).toUpperCase()}{' '}
+							</Text>
+						</TouchableHighlight>
+					</View>
 
-				<View style={styles.row}>
-					<View style={styles.item}>
-						<Text style={styles.label}>First Name:</Text>
-						<Text style={styles.input}>{state.user.attributes.first_name}</Text>
+					<View style={styles.row}>
+						<View style={styles.item}>
+							<Text style={styles.label}>First Name:</Text>
+							<Text style={styles.input}>{user.attributes.first_name}</Text>
+						</View>
+						<View style={styles.item}>
+							<Text style={styles.label}>Last Name:</Text>
+							<Text style={styles.input}>{user.attributes.last_name}</Text>
+						</View>
 					</View>
-					<View style={styles.item}>
-						<Text style={styles.label}>Last Name:</Text>
-						<Text style={styles.input}>{state.user.attributes.last_name}</Text>
+					<View style={styles.row}>
+						<View style={styles.item}>
+							<Text style={styles.label}>Gender:</Text>
+							<Text style={styles.input}>{user.attributes.gender}</Text>
+						</View>
+						<View style={styles.item}>
+							<Text style={styles.label}>Phone Number:</Text>
+							<Text style={styles.input}>{user.attributes.phone_number}</Text>
+						</View>
 					</View>
-				</View>
-				<View style={styles.row}>
-					<View style={styles.item}>
-						<Text style={styles.label}>Gender:</Text>
-						<Text style={styles.input}>{state.user.attributes.gender}</Text>
-					</View>
-					<View style={styles.item}>
-						<Text style={styles.label}>Phone Number:</Text>
-						<Text style={styles.input}>
-							{state.user.attributes.phone_number}
-						</Text>
-					</View>
-				</View>
-				<View style={styles.row}>
-					<View style={styles.address}>
-						<Text style={styles.label}>Email Address:</Text>
-						<Text style={styles.email}>
-							{state.user.attributes.email_address}
-						</Text>
+					<View style={styles.row}>
+						<View style={styles.address}>
+							<Text style={styles.label}>Email Address:</Text>
+							<Text style={styles.email}>{user.attributes.email_address}</Text>
+						</View>
 					</View>
 				</View>
-			</View>
+			)}
 		</View>
 	);
 }
 
 const styles = StyleSheet.create({
-  row: {
-    flexDirection: "row",
-    backgroundColor: "#EFF5F9",
-    marginLeft: 30,
-    marginRight: 24,
-    marginTop: 40,
-    marginBottom: 10,
-    justifyContent: "space-between",
-    alignItems: "center",
-  },
-  address: {
-    backgroundColor: "#EFF5F9",
-    marginRight: 24,
-  },
-  item: {
-    backgroundColor: "#EFF5F9",
-    width: 140,
-  },
-  label: {
-    color: "#111",
-    fontFamily: "Montserrat_700Bold",
-    marginBottom: 5,
-  },
-  input: {
-    backgroundColor: "#E8EBF7",
-    borderRadius: 6,
-    color: "#72788D",
-    fontFamily: "Montserrat_600SemiBold",
-    padding: 9,
-  },
-  email: {
-    backgroundColor: "#E8EBF7",
-    borderRadius: 6,
-    color: "#72788D",
-    fontFamily: "Montserrat_600SemiBold",
-    padding: 9,
-    width: Dimensions.get("window").width * 0.86,
-  },
-  container: {
-    flex: 1,
-    height: "100%",
-    position: "relative",
-  },
-  hamburger: {
-    marginTop: 80,
-    marginRight: 24,
-  },
-  cards: {
-    backgroundColor: "#EFF5F9",
-    flexDirection: "column",
-    alignItems: "center",
-  },
-  header: {
-    flex: 1,
-    flexDirection: "row",
-    justifyContent: "space-between",
-    backgroundColor: "#EFF5F9",
-  },
-  main: {
-    flex: 3,
-    backgroundColor: "#EFF5F9",
-  },
-  name: {
-    marginHorizontal: 30,
-    fontSize: 25,
-    color: "#074A74",
-    fontFamily: "Montserrat_700Bold",
-  },
+	row: {
+		flexDirection: 'row',
+		backgroundColor: '#EFF5F9',
+		marginLeft: 30,
+		marginRight: 24,
+		marginTop: 40,
+		marginBottom: 10,
+		justifyContent: 'space-between',
+		alignItems: 'center',
+	},
+	address: {
+		backgroundColor: '#EFF5F9',
+		marginRight: 24,
+	},
+	item: {
+		backgroundColor: '#EFF5F9',
+		width: 140,
+	},
+	label: {
+		color: '#111',
+		fontFamily: 'Montserrat_700Bold',
+		marginBottom: 5,
+	},
+	input: {
+		backgroundColor: '#E8EBF7',
+		borderRadius: 6,
+		color: '#72788D',
+		fontFamily: 'Montserrat_600SemiBold',
+		padding: 9,
+	},
+	email: {
+		backgroundColor: '#E8EBF7',
+		borderRadius: 6,
+		color: '#72788D',
+		fontFamily: 'Montserrat_600SemiBold',
+		padding: 9,
+		width: Dimensions.get('window').width * 0.86,
+	},
+	container: {
+		flex: 1,
+		height: '100%',
+		position: 'relative',
+	},
+	hamburger: {
+		marginTop: 80,
+		marginRight: 24,
+	},
+	cards: {
+		backgroundColor: '#EFF5F9',
+		flexDirection: 'column',
+		alignItems: 'center',
+	},
+	header: {
+		flex: 1,
+		flexDirection: 'row',
+		justifyContent: 'space-between',
+		backgroundColor: '#EFF5F9',
+	},
+	main: {
+		flex: 3,
+		backgroundColor: '#EFF5F9',
+	},
+	name: {
+		marginHorizontal: 30,
+		fontSize: 25,
+		color: '#074A74',
+		fontFamily: 'Montserrat_700Bold',
+	},
 
-  menu: {
-    position: "absolute",
-    right: 0,
-  },
+	menu: {
+		position: 'absolute',
+		right: 0,
+	},
 
-  modalContainer: {
-    height: Dimensions.get("screen").height / 2.1,
-    alignItems: "center",
-    marginTop: "auto",
-    borderRadius: 15,
-  },
-  modalContent: {
-    paddingVertical: 20,
-    alignItems: "center",
-  },
-  modalHeading: {
-    fontFamily: "Montserrat_700Bold",
-    fontSize: 30,
-  },
-  modalHeaderCloseText: {
-    backgroundColor: "white",
-    textAlign: "center",
-    paddingLeft: 5,
-    paddingRight: 5,
-    width: 30,
-    fontSize: 15,
-    borderRadius: 50,
-  },
+	modalContainer: {
+		height: Dimensions.get('screen').height / 2.1,
+		alignItems: 'center',
+		marginTop: 'auto',
+		borderRadius: 15,
+	},
+	modalContent: {
+		paddingVertical: 20,
+		alignItems: 'center',
+	},
+	modalHeading: {
+		fontFamily: 'Montserrat_700Bold',
+		fontSize: 30,
+	},
+	modalHeaderCloseText: {
+		backgroundColor: 'white',
+		textAlign: 'center',
+		paddingLeft: 5,
+		paddingRight: 5,
+		width: 30,
+		fontSize: 15,
+		borderRadius: 50,
+	},
 });
