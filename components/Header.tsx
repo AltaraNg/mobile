fbd0e7b3a5b2a8762cfecd2a3d0296aea6207021
Adeniyi <teebarg01@gmail.com>
2023-10-22 18:49:11 +0100

    build setup

--- components/Header.tsx
+++ components/Header.tsx
@@ -1,15 +1,11 @@
-import { StyleSheet } from 'react-native';
+import React from "react";
+import { View } from "../components/Themed";
+import SVGImage from "../assets/svgs/splash.svg";
 
-import EditScreenInfo from '../components/EditScreenInfo';
-import { Text, View } from '../components/Themed';
-import SVGImage from '../assets/svgs/splash.svg';
-
-
-export default function Header(props:any) {
-	return (
-		<View style={{backgroundColor:props.backgroundColor, paddingTop:60, paddingLeft:24}}  >
-			<SVGImage  width={150} height={75} />
-		</View>
-	);
+export default function Header(props) {
+    return (
+        <View style={{ backgroundColor: props.backgroundColor, paddingTop: 60, paddingLeft: 24 }}>
+            <SVGImage width={150} height={75} />
+        </View>
+    );
 }
-
