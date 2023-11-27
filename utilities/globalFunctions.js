47846c4f05ce860479f97501844b68590be7b602
Blessing Onwunali <cheedinmarblessing@gmail.com>
2023-11-27 15:14:06 +0100

    fix: remove console.log

--- utilities/globalFunctions.js
+++ utilities/globalFunctions.js
@@ -1,19 +1,15 @@
 import axios from "axios";
 
 export const logActivity = async (token, activity_id) => {
-    try {
-        await axios({
-            method: "POST",
-            url: `/app/audit`,
-            headers: { Authorization: `Bearer ${token}` },
-            data: {
-                mobile_app_activity_id: activity_id,
-                meta: {},
-            },
-        });
-    } catch (error) {
-        console.log(error);
-    }
+    await axios({
+        method: "POST",
+        url: `/app/audit`,
+        headers: { Authorization: `Bearer ${token}` },
+        data: {
+            mobile_app_activity_id: activity_id,
+            meta: {},
+        },
+    });
 };
 
 export const formatAsMoney = (figure) => {
